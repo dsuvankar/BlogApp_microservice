6b7c1635b4a7bec3da78a51f10c3028825233583
suvankar <iamsuvankar25@gmail.com>
2026-02-27 21:38:33 +0530

    fix: correct environment variable names for Google OAuth configuration

--- backend/userService/src/utils/GoogleConfig.ts
+++ backend/userService/src/utils/GoogleConfig.ts
@@ -3,10 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET;
-console.log("Google Client ID:", GOOGLE_CLIENT_ID);
-console.log("Google Client Secret:", GOOGLE_CLIENT_SECRET);
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 export const oauth2client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
